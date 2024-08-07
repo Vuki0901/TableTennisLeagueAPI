7f32c77164903143349c67623ed38a48cef251b9
Leo Vukoje <leo.vukoje02@gmail.com>
2024-08-07 20:19:15 +0200

    WIP.

---
+++ Persistence/Migrations/20240807170623_AlterLeagueInvitationTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterLeagueInvitationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InvitationToken",
+                table: "LeagueInvitations",
+                newName: "PlayerEmailAddress");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayerEmailAddress",
+                table: "LeagueInvitations",
+                newName: "InvitationToken");
+        }
+    }
+}
